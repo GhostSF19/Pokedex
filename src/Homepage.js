22c2004dfc3b6a3416a4b0213c59b19d770639b6
GhostSF19 <156846190+GhostSF19@users.noreply.github.com>
2024-08-04 00:45:56 +0200

    ImgsAdded

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,28 +3,26 @@ import './Homepage.css';
 
 const Homepage = () => {
   return (
-    <div className="homepage d-flex align-items-center justify-content-end">
-      <div className="text-container p-3">
-        <h1>by Denis</h1>
-      </div>
-      <div className="image-container">
-      <img src="/PokeBall1.png" alt="PokeBall" style={{ width: '166.97px', height: '167.55px', transform: 'rotate(4.41deg)', opacity: 0.9 }} />
-
-        <img src="/Vector.png" alt="Vector" className="vector img-fluid" />
-        <img src="/Pikachu.png" alt="Pikachu" className="pikachu img-fluid" />
+    <div className="homepage container-fluid">
+      <nav className="navbar navbar-light navbar-custom">
+        <a className="nav-link" href="#">Home</a>
+        <a className="nav-link" href="#">Pokédex</a>
+      </nav>
+      <div className="row h-100">
+        <div className="col-12 d-flex justify-content-end align-items-end text-container">
+          <h1>by Denis</h1>
+        </div>
+        <div className="col-12 d-flex justify-content-end align-items-end image-container">
+          <img src="/PokeBall1.png" alt="PokeBall1" className="pokeball1 img-fluid" />
+          <img src="/Pokeball2.png" alt="Pokeball2" className="pokeball2 img-fluid" />
+          <img src="/Vector.png" alt="Vector" className="vector img-fluid" />
+          <img src="/Vector2.png" alt="Vector2" className="vector2 img-fluid" />
+          <img src="/Pikachu.png" alt="Pikachu" className="pikachu img-fluid" />
+          <img src="/poklogo.png" alt="Poklogo" className="poklogo img-fluid" />
+        </div>
       </div>
     </div>
   );
 };
 
 export default Homepage;
-
-
-
-
-
-
-
-
-
-
