8339f562ac14de0ecc4831e23bb5eeffb80ef2a1
GhostSF19 <156846190+GhostSF19@users.noreply.github.com>
2024-08-27 22:20:18 +0200

    Mobilefirst01

--- src/Homepage.js
+++ src/Homepage.js
@@ -3,11 +3,9 @@ import './Homepage.css';
 import PokeButton from './components/shared/pokebutton';
 
 const Homepage = () => {
-
-
   const handleClick = () => {
     alert('Button clicked!');
-  };  
+  };
 
   return (
     <div className="homepage">
@@ -15,31 +13,32 @@ const Homepage = () => {
         <div className="px-5 col-5">
           <img src="/poklogo.png" alt="Poklogo" className="poklogo img-fluid" />
         </div>
-        <div className="px-5 row d-flex flex-nowrap justify-content-end col-5 col-md-2"  >
-          <a className="nav-link" href="#">
-            Home
-          </a>
-          <a className="nav-link" href="#">
-            Pokédex
-          </a>
+        <div className="px-5 d-flex justify-content-end col-5 col-md-2">
+          <a className="nav-link" href="#">Home</a>
+          <a className="nav-link" href="#">Pokédex</a>
         </div>
       </div>
-      <div className="row h-100">
-        <div className="col-12 d-flex justify-content-end align-items-end text-container">
-          <PokeButton onClick={handleClick}  text={"See Pokemons"} />
-          <h1>by Denis</h1>
-        </div>
-        <div className="col-12 d-flex justify-content-end align-items-end image-container">
+      <div className="d-flex flex-column align-items-center justify-content-center h-100">
+        <div className="text-center mb-4">
           <img
             src="/banner.png"
             alt="PokeBall1"
             className="pokeball1 img-fluid"
           />
         </div>
+        <div className="text-center mb-4">
+          <h1 className="custom-heading">
+            <strong>Find</strong> all your <br /> favorite <br /> <strong>Pokemon</strong>
+          </h1>
+          <h4 className="text-muted">
+            You can know the type of Pokemon, its strengths, disadvantages and abilities
+          </h4>
+        </div>
+        <PokeButton onClick={handleClick} text={"See Pokemons"} />
       </div>
+      <div className="text-center mt-4"><h4>by Denis</h4></div>
     </div>
   );
 };
 
 export default Homepage;
-
