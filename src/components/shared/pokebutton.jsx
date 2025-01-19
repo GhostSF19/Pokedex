85a76313672fceaa98bb36fd7134fe24ce39b4d1
GhostSF19 <156846190+GhostSF19@users.noreply.github.com>
2025-01-19 23:24:41 +0100

    Updated404errorPage

--- src/components/shared/pokebutton.jsx
+++ src/components/shared/pokebutton.jsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
-const PokeButton = ({ onClick, text, type = 'button', disabled = false }) => {
-    return (
-        <button
-            type={type}
-            onClick={onClick}
-            disabled={disabled}
-            className="btn btn-primary poke-button btn-lg btn-block"
-        >
-            {text}
-        </button>
-    );
+const PokeButton = ({ onClick, text, type = 'button', disabled = false, className = '' }) => {
+  return (
+    <button
+      type={type}
+      onClick={onClick}
+      disabled={disabled}
+      className={`btn btn-primary poke-button ${className}`} // Added dynamic className
+    >
+      {text}
+    </button>
+  );
 };
 
 PokeButton.propTypes = {
-    onClick: PropTypes.func.isRequired,
-    text: PropTypes.node.isRequired,
-    type: PropTypes.oneOf(['button', 'submit', 'reset']),
-    disabled: PropTypes.bool,
-    className: PropTypes.string,
+  onClick: PropTypes.func.isRequired,
+  text: PropTypes.node.isRequired,
+  type: PropTypes.oneOf(['button', 'submit', 'reset']),
+  disabled: PropTypes.bool,
+  className: PropTypes.string,
 };
 
 export default PokeButton;
--- src/components/views/404errorpage.js
+++ src/components/views/404errorpage.js
@@ -1,9 +1,11 @@
+import React from 'react';
+import PokeButton from '../shared/pokebutton';  // Import the reusable button
 import '../../styles/404errorpage.css';
 
 const Error404Page = () => {
   return (
     <div className="error-page-container text-center py-5">
-      {/* Images will stay in place */}
+      {/* Images */}
       <div className="error-images rocket-container">
         <img
           src="/Team_Rocket_trio_OS%201.png"
@@ -12,7 +14,6 @@ const Error404Page = () => {
         />
       </div>
 
-      {/* 404 Background Image */}
       <div className="error-images background-container">
         <img
           src="/404.png"
@@ -21,13 +22,23 @@ const Error404Page = () => {
         />
       </div>
 
-      {/* Text and Button Container */}
-      <div className="text-and-button-container mt-5">
+      {/* Text Container */}
+      <div className="error-text-container">
         <h1 className="display-1 text-danger">404</h1>
-        <p className="lead">The rocket team has won this time.</p>
-        <a href="/" className="btn btn-primary">
-          Return
-        </a>
+        <div className="text-block">
+          <span className="text-white d-block rocket-text-large">The rocket team</span>
+          <span className="text-black d-block rocket-text-large">has won this time</span>
+        </div>
+      </div>
+
+      {/* Button */}
+      <div className="error-button-container">
+        {/* Only one button here with custom class */}
+        <PokeButton 
+          onClick={() => window.location.href = '/'} 
+          text="Return" 
+          className="poke-button-second"  // Optional: Custom class for styling
+        />
       </div>
     </div>
   );
